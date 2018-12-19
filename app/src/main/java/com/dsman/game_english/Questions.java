9c75d17274acc0d9fb1c62dca21c327479313388
DSMan97 <guillecrespoaguayo@gmail.com>
2018-12-20 00:45:36 +0100

    Primer Commit

--- app/src/main/java/com/dsman/game_english/Questions.java
+++ app/src/main/java/com/dsman/game_english/Questions.java
@@ -3,8 +3,10 @@ package com.dsman.game_english;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Questions extends AppCompatActivity {
+import java.util.List;
 
+public class Questions extends AppCompatActivity {
+    private List<String> preguntas = new List<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
