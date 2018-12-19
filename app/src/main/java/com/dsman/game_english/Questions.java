3ea7e182ad895df51aef3e83aff177a4b56976ab
vicex99 <v.a.lantaron@gmail.com>
2018-12-20 00:55:44 +0100

    modif List

--- app/src/main/java/com/dsman/game_english/Questions.java
+++ app/src/main/java/com/dsman/game_english/Questions.java
@@ -3,13 +3,18 @@ package com.dsman.game_english;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Questions extends AppCompatActivity {
-    private List<String> preguntas = new List<String>();
+    private List<String> preguntas = new ArrayList<>();
+    private List<String []> prespuestas = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+
     }
 }
